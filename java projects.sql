86097ba6658149a383d057e062fe0aa0ee3c4c95
suyog <mgrsuyog6@gmail.com>
2023-12-17 18:29:14 +0545

    database file

---
+++ java projects.sql
@@ -0,0 +1,32 @@
+-- create database suyog;
+-- use suyog;
+-- create table Student(roll int, name varchar(50), course varchar(50));
+-- insert into Student(roll,name,course) values(1,"Suyog Rana Magar","BSC.Csit");
+-- select *from Student;
+
+
+-- select * from Student;
+-- insert into Student(roll,course) values(2,'csit');
+-- alter table student modify name varchar(50) not null;
+
+create database bankmanagementsystem;
+
+use bankmanagementsystem;
+
+create table signup(form_number varchar(50), name varchar(50), fathers_name varchar(50), dob varchar(50), gender varchar(50), email varchar(50), martial varchar(50), address varchar(50), city varchar(50), state varchar(50), pin varchar(50));
+show tables;
+select * from signup;
+
+create table signup2(form_number varchar(40), religion varchar(50), catogery varchar(50), income varchar(50), qualification varchar(50), occupation varchar(50), senior varchar(50), account varchar(50), pan varchar(10), adhar varchar(50));
+select * from signup2;
+ 
+create table signup3(form_number varchar(50), accountType varchar(50), cardNum varchar(50), pinNum varchar(50), facility varchar(50));
+create table login(form_number varchar(50), cardNum varchar(50), pinNum varchar(50));
+select * from signup3;
+select * from login;
+-- DROP table login;
+
+
+create table bank(pinNum varchar(50), date varchar(50), type varchar(50), amount varchar(50));
+select * from bank;
+DROP table bank;
